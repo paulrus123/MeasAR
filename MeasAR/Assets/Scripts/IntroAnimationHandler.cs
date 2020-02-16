c6ac866fb74d326c91de42ee8693528a62bdcd05
Paul Sorenson <psoren@umich.edu>
2020-02-16 13:58:47 -0500

    Added intro animation

---
+++ MeasAR/Assets/Scripts/IntroAnimationHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroAnimationHandler : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 5.0f);
+    }
+}
